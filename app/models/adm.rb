629db2724443e2f4c0317a05db561d4e1bb98247
ribas858 <ribas858@gmail.com>
2018-06-22 23:27:28 -0300

    env

--- app/models/adm.rb
+++ app/models/adm.rb
@@ -3,4 +3,8 @@ class Adm < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         protected
+def confirmation_required?
+  false
+end
 end
