87d335835e34fb492934d4a192da5ae69bca1aa9
gabrielevi10 <gabrielevigomes@gmail.com>
2018-10-16 21:09:17 -0300

    Adiciona .yml pra bd geral

--- db/schema.rb
+++ db/schema.rb
@@ -18,23 +18,15 @@ ActiveRecord::Schema.define(version: 2018_06_21_132144) do
   create_table "adms", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_adms_on_confirmation_token", unique: true
     t.index ["email"], name: "index_adms_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_adms_on_reset_password_token", unique: true
   end
 
   create_table "newsletters", force: :cascade do |t|
